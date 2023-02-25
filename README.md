f0343486b55e5f8dd1240691a9cf8cd29eadb704
Theo Alves Da Costa <theo.alvesdacosta@ekimetrics.com>
2023-02-25 16:05:01 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# bechdelai-tool-image
-BechdelAI demo tool - Analyze images
+---
+title: Bechdelai Image
+emoji: 🌖
+colorFrom: green
+colorTo: yellow
+sdk: gradio
+sdk_version: 3.10.1
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,38 @@
+import gradio as gr
+import os
+
+from bechdelai.image.face_detection import FacesDetector
+from bechdelai.image.gender_detection import GenderDetector
+from bechdelai.image.vilt import ViLT
+from bechdelai.image.img import Img
+
+fd = FacesDetector()
+gd = GenderDetector()
+
+examples_path = "static/examples"
+examples_images = [os.path.join(examples_path,x) for x in os.listdir(examples_path)]
+
+def analyze_genders(img):
+    rois,faces = fd.detect(img,method = "retinaface",padding = 20)
+    probas = gd.predict(faces)
+    results,metrics = gd.analyze_probas(probas)
+    img_with_faces = fd.show_faces_on_image(img,rois,width = 3,genders = probas["gender"])
+
+    new_metrics = {"Space occupied by women":metrics["women_area"],"Space occupied by men":metrics["men_area"]}
+
+    return img_with_faces,new_metrics
+
+
+demo = gr.Interface(
+    fn = analyze_genders, 
+    inputs = gr.Image(), 
+    outputs = [
+        gr.outputs.Label(num_top_classes=3),
+        "image",
+    ],
+    examples=examples_images,
+    title="BechdelAI Tool - Image Analysis",
+    description="BechdelAI automates the Bechdel test and study of women under-representation in cinema and media with AI",
+    article="For more information visit https://github.com/dataforgoodfr/bechdelai"
+)
+demo.launch()
